ee4213ec64f085c726470d90b463a11c3312ab11
Prateek Chaudhary <prateek.pc07@gmail.com>
2019-08-19 23:22:02 +0530

    even odd

---
+++ even_odd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int number;
+	printf("enter any number\n");
+	scanf("%d", &number);
+	if(number%2==0)
+	{
+		printf("number is even\n");
+	}
+	else
+	{
+		printf("number is odd\n");
+	}
+	return 0;
+}
